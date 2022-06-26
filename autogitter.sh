53f87210930732b7b2a9009b3e90739cb00f2e5d
themataleao <willhausmarc@gmail.com>
2022-06-26 21:15:02 +0000

    this is a trial

--- autogitter.sh
+++ autogitter.sh
@@ -1,5 +1,5 @@
 cd /root/autogitter
 filename=`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32`
-filesize=10737
+filesize=1073
 head -c $filesize </dev/urandom > "./random/$filename"
 ./gitpusher.sh
