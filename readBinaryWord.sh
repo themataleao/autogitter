89d2e0a57927d2f9eae5d0d2507f7ae8db43b8fb
themataleao <willhausmarc@gmail.com>
2022-06-26 21:20:21 +0000

    this is a trial

--- readBinaryWord.sh
+++ readBinaryWord.sh
@@ -3,16 +3,18 @@ binaryWord=`cat binarizedWord.txt`
 for (( i=0; i<${#binaryWord}; i++ )); do
   echo "${binaryWord:$i:1}"
   pushOrNot=${binaryWord:$i:1}
-  sleep 10
   ./autogitter.sh
   if [ "$pushOrNot" == "1" ]; then
     for i in {1..20}
     do
       echo "Push it..."
       ./autogitter.sh
-       sleep 10
+      MINWAIT=10
+      MAXWAIT=30
+      sleep $((MINWAIT+RANDOM % (MAXWAIT-MINWAIT)))
     done
   else
     echo "...to the limit"
   fi
+  sleep 10
 done
