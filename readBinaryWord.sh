8037271d7478b47db5ac0861b2bb35db03b62fe2
themataleao <willhausmarc@gmail.com>
2022-06-26 21:12:06 +0000

    this is a trial

--- readBinaryWord.sh
+++ readBinaryWord.sh
@@ -2,6 +2,16 @@
 binaryWord=`cat binarizedWord.txt`
 for (( i=0; i<${#binaryWord}; i++ )); do
   echo "${binaryWord:$i:1}"
+  pushOrNot=`${binaryWord:$i:1}`
   sleep 10
   ./autogitter.sh
+  if [ "$pushOrNot" == "1" ]; then
+    for i in {1..20}
+    do
+      sleep 10
+      ./autogitter.sh
+    done
+  else
+    echo "This day was skipped"
+  fi
 done
