c5abeda8794c96bd014a343c598b11a615d1f45b
themataleao <willhausmarc@gmail.com>
2022-07-05 19:19:45 +0000

    push it to the limmit

--- autogitter.sh
+++ autogitter.sh
@@ -2,5 +2,7 @@
 cd /root/autogitter
 filename=`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32`
 filesize=1073
-head -c $filesize </dev/urandom > "./random/$filename"
+formats=("ts" "js" "css" "html" "py" "c" "class" "cpp" "java" "php" "py" "sh" "swift" "yml" "r" "cuda" )
+format=${formats[ $RANDOM % ${#formats[@]} ]}
+head -c $filesize </dev/urandom > "./random/$filename.$format"
 ./gitpusher.sh
